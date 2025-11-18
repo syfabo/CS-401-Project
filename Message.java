2db1caf0c2a9c44dd02dfeda9e08bbb36080b7a7
Sydney <sboone@horizon.csueastbay.edu>
2025-11-18 00:35:00 -0800

    updated classes + added data files
    Added more details to messages for distinction.
    Started server logic, added methods and attributes.

--- Message.java
+++ Message.java
@@ -8,22 +8,52 @@ public class Message implements Serializable {
 	private static int count = 0;
 	private final int id;
 	private final MessageType type; 	  // login, text, logout, undefined
-    private final MessageStatus status;  // success, error, request, lowercase, uppercase
+    private final MessageStatus status;                // success, error, request, lowercase, uppercase
     private final String text;
+    private final long number;
+    private final Application sender;
     
     // messages with no parameters
     public Message() {
     	this.id = ++count;
         this.type = MessageType.undefined;
         this.status = MessageStatus.undefined;
-        this.text = "undefined"; 
+        this.text = "undefined";
+        this.number = 0;
+        this.sender = Application.undefined;
     }
     
-    public Message(MessageType type, MessageStatus status, String text) {
+    public Message(MessageStatus status, MessageType type, Application app, int num, String text) {
     	this.type = type;
     	this.status = status;
     	this.text = text;
     	this.id = ++count;
+    	this.number = num;
+    	this.sender = app;
+    }
+
+    // getters
+    public int getID() {
+    	return id;
+    }
+    
+    public MessageType getType() {
+    	
+    	return type;
+    }
+    
+    public MessageStatus getStatus() {
+    	
+    	return status;
+    }
+    
+    public String getText() {
+    	
+    	return text;
+    }
+    
+    public long getNum() {
+    	return number;
     }
 
     // getters
