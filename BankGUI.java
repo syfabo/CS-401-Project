5b1b3df913dfbe2a2221da5cabf8b5bfd6b493d5
Sydney <sboone@horizon.csueastbay.edu>
2025-11-20 12:23:31 -0800

    GUI Class
    created GUI class

---
+++ BankGUI.java
@@ -0,0 +1,18 @@
+package group3;
+
+import java.util.Scanner;
+
+public class BankGUI {
+	Profile profile;
+	Account account;
+	Scanner scan;
+	
+	public BankGUI() {
+		scan = new Scanner(System.in);
+	}
+	
+	
+	
+	
+
+}
