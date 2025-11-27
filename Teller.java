0f0ac55182fa4b54f7972ac4a56feb8a1b2a5c61
Sydney <sboone@horizon.csueastbay.edu>
2025-11-26 22:04:11 -0800

    GUI login screens for teller app
    * code test
    
    * Server, Client apps, and Messages
    
    * Server, Client apps, and Messages
    
    * class updates and data files
    
    * Class Updates
    
    * Added code that demonstrates client server is working
    
    * added more functionality to server and teller, implemented Profile and Account
    
    * Gui Class
    
    * Bank GUI start
    
    * BankGUI start pages
    
    * GUI updates
    
    * uploading entire java project
    
    * removing java project
    
    * GUI progress + other class updates
    
    * removing extra files
    
    * GUI login screens for teller

--- Teller.java
+++ Teller.java
@@ -5,37 +5,37 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 
 public class Teller {
 	// easy access message parameters
+		static MessageType employeelogin = MessageType.employeeLogin;
 		static MessageStatus request = MessageStatus.request;
 		static MessageType withdraw = MessageType.withdrawal;
 		static MessageType deposit = MessageType.deposit;
-		static MessageType login = MessageType.login;
 		static MessageType logout = MessageType.logout;
 		static MessageType updateAcc = MessageType.updateAccount;
 		static MessageType updateProf = MessageType.updateProfile;
+		static MessageType customerlogin = MessageType.customerLogin;
 		Scanner scan = new Scanner(System.in); // temporary, we will use GUI for input
+		
+		public Teller(String ip) throws IOException {
+			int port = 777;
+			try(
+			// Socket that connects to the ip and port
+			Socket socket = new Socket(ip, port);
+							
+			// create the object input and output streams on socket
+			ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+			ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());){}
+		}
 	
 		
-	public static void main(String args[]) throws IOException{
-		// get user ip
-		String ip = InetAddress.getLocalHost().getHostAddress();
-		int port = 777;
-		
-	
-		// Socket that connects to the ip and port
-		Socket socket = new Socket(ip, port);
-		System.out.println("Connected to " + ip + ":" + port);
-						
-		// create the object input and output streams on socket
-		ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-		ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-	
-	
-		
-	
-	
+	public static void main(String args[]) throws IOException {
+		String ip = JOptionPane.showInputDialog("Enter IP Address"); //TODO maybe handle exit
+		Teller teller = new Teller(ip);
+		BankGUI gui = new BankGUI(null, teller);
 	}
 	
 	public void start() {
@@ -101,6 +101,21 @@ public class Teller {
 		return valid;
 		
 	}
+	
+	// checks with server and returns profile if user information
+		public Boolean customerLogin(String user, String pass) {
+			boolean valid = false;
+			
+			// create + send request message to teller login with user and pass
+			
+			// listen for confirm message
+			
+			// if status == confirmation valid = true return valid
+			
+			// else 
+			return valid;
+			
+		}
 	
 	
 	// authenticates user login info and returns null if invalid
